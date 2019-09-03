37b65e17a98cc736f8f59d2ecbff148de19ad369
unknown <vkchang39@gmail.com>
2019-09-04 02:20:32 +0530

    Adding daily summary, high and low temp and minor bug fixes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ request( { url, json: true}, (error, {body} = {}) => {
     } else if(body.error) {
         callback('Location not found!', undefined)
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. ' + body.daily.summary)
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. Max temperatur today will be ' + body.daily.data[0].temperatureHigh + ' and lowest temperature will be ' + body.daily.data[0].temperatureLow + ' There is a ' + body.currently.precipProbability + '% chance of rain. ' + body.daily.summary)
         
     }
         
